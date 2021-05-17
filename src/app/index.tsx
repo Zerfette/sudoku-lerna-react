c6b1a9a9e857d938aeb1ec281a953b56ea022df3
Sarah <srenfro00@gmail.com>
2021-05-17 09:50:28 -0500

    1.0.0

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,20 +3,18 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { ChakraProvider, theme } from '@chakra-ui/react'
 import { map } from 'fp-ts/Array'
-import { pipe } from 'fp-ts/function'
 import store from '~core'
 import { routes } from '~routes'
 
-const ToRoute: FC<{ path: string; text: string; component: FC }> = ({
-  path,
-  text,
-  component
-}) => <Route key={text} path={path} component={component} />
+type Props = { path: string; text: string; component: FC }
+const ToRoute: FC<Props> = ({ path, text, component }) => (
+  <Route key={text} path={path} component={component} />
+)
 
-export const App: FC = () => (
+export const App = () => (
   <Provider store={store}>
     <ChakraProvider theme={theme}>
-      <BrowserRouter>{pipe(routes, map(ToRoute))}</BrowserRouter>
+      <BrowserRouter>{map(ToRoute)(routes)}</BrowserRouter>
     </ChakraProvider>
   </Provider>
 )
