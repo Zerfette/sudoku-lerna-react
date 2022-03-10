1946a5c339174b75cd3fce11b01b14e7143774f1
Sarah <srenfro00@gmail.com>
2022-03-09 18:31:43 -0600

    Rename getAvailableValues

--- src/store/board/selectors.ts
+++ src/store/board/selectors.ts
@@ -1,10 +1,10 @@
 import { createSelector } from 'reselect'
 import { boardLens } from 'core/optics'
-import { getAvailablesValues, getSelectedOption } from 'core/selectors'
+import { getAvailableValues, getSelectedOption } from 'core/selectors'
 
 export const getSelected = createSelector([boardLens.get], getSelectedOption)
 
 export const getAvailables = createSelector(
   [boardLens.get],
-  getAvailablesValues
+  getAvailableValues
 )
